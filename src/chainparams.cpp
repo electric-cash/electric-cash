d635c4d4033bea1d742f712aa1bca351f720197a
mtwaro <57895109+mtwaro@users.noreply.github.com>
2021-01-22 16:26:11 +0100

    Remove comment

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -122,7 +122,6 @@ public:
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
 
-        // TODO: uncomment this when ready
         vSeeds.emplace_back("seed.electriccash.global");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,33);
