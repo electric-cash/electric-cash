b67f9c960b6af18a81145c14d6fcde2fa908f071
Mariusz Twarowski <f20f1a85b3@protonmail.com>
2021-06-21 19:23:28 +0200

    Fix staking tx parser unit tests

--- src/test/staking_tests.cpp
+++ src/test/staking_tests.cpp
@@ -197,7 +197,7 @@ BOOST_AUTO_TEST_CASE(recognize_invalid_deposit_tx_corrupted_varint)
     BOOST_AUTO_TEST_CASE(recognize_valid_burn_tx)
     {
         const size_t stakingHeaderSize = 11;
-        const unsigned char stakingBurnHeader[stakingHeaderSize] = {OP_RETURN, STAKING_TX_HEADER, STAKING_TX_BURN_SUBHEADER, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01};
+        const unsigned char stakingBurnHeader[stakingHeaderSize] = {OP_RETURN, STAKING_TX_HEADER, STAKING_TX_BURN_SUBHEADER, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00}; // 42.94967296 ELC to be burned
         CScript stakingTxHeader = CScript(stakingBurnHeader, stakingBurnHeader + stakingHeaderSize);
         CScript dummyScript = CScript() << OP_DUP << OP_HASH160 << ToByteVector(dummyPubkey.GetID()) << OP_EQUALVERIFY << OP_CHECKSIG;
 
@@ -214,10 +214,29 @@ BOOST_AUTO_TEST_CASE(recognize_invalid_deposit_tx_corrupted_varint)
         BOOST_CHECK(txType == StakingTransactionType::BURN);
     }
 
+    BOOST_AUTO_TEST_CASE(recognize_invalid_burn_tx_invalid_amount)
+    {
+        const size_t stakingHeaderSize = 11;
+        const unsigned char stakingBurnHeader[stakingHeaderSize] = {OP_RETURN, STAKING_TX_HEADER, STAKING_TX_BURN_SUBHEADER, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x00}; // 22.5 M ELC to burn
+        CScript stakingTxHeader = CScript(stakingBurnHeader, stakingBurnHeader + stakingHeaderSize);
+        CScript dummyScript = CScript() << OP_DUP << OP_HASH160 << ToByteVector(dummyPubkey.GetID()) << OP_EQUALVERIFY << OP_CHECKSIG;
+
+        CMutableTransaction depositTx;
+        depositTx.nVersion = 1;
+        depositTx.vin.resize(1);
+        depositTx.vout.resize(2);
+        depositTx.vout[0].scriptPubKey = stakingTxHeader;
+        depositTx.vout[1].nValue = 3 * COIN;
+        depositTx.vout[1].scriptPubKey = dummyScript;
+        CStakingTransactionParser stakingTxParser(MakeTransactionRef(CTransaction(depositTx)));
+        StakingTransactionType txType = stakingTxParser.GetStakingTxType();
+        BOOST_CHECK(txType == StakingTransactionType::NONE);
+    }
+
     BOOST_AUTO_TEST_CASE(recognize_invalid_burn_tx_corrupted_amount)
     {
         const size_t stakingHeaderSize = 10;
-        const unsigned char stakingBurnHeader[stakingHeaderSize] = {OP_RETURN, STAKING_TX_HEADER, STAKING_TX_BURN_SUBHEADER, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01};
+        const unsigned char stakingBurnHeader[stakingHeaderSize] = {OP_RETURN, STAKING_TX_HEADER, STAKING_TX_BURN_SUBHEADER, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00}; // script too short
         CScript stakingTxHeader = CScript(stakingBurnHeader, stakingBurnHeader + stakingHeaderSize);
         CScript dummyScript = CScript() << OP_DUP << OP_HASH160 << ToByteVector(dummyPubkey.GetID()) << OP_EQUALVERIFY << OP_CHECKSIG;
 
