b9e708c00c96e6213b592a600754f51579a55ada
Mariusz Twarowski <f20f1a85b3@protonmail.com>
2021-06-24 12:28:47 +0200

    Refactor staking burn test

--- test/functional/feature_staking_burn.py
+++ test/functional/feature_staking_burn.py
@@ -1,3 +1,5 @@
+import decimal
+
 from test_framework.messages import COIN
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import (
@@ -19,6 +21,38 @@ class StakingBurnTest(BitcoinTestFramework):
     def get_staking_pool_balance(self, node_num: int) -> int:
         return self.nodes[node_num].getstakinginfo()['staking_pool']
 
+    @staticmethod
+    def create_staking_burn_tx_input(change_address: str, amount_to_burn: decimal.Decimal, utxo: dict):
+        fee = COIN // 1000
+        tx_input = {
+            "txid": utxo["txid"],
+            "vout": utxo["vout"]
+        }
+        # tx outputs
+        amount_to_burn_bin = int(amount_to_burn).to_bytes(8, "little")
+        tx_output_staking_burn_header = {
+            "data": (bytes([STAKING_TX_HEADER, STAKING_TX_BURN_SUBHEADER]) + amount_to_burn_bin).hex()
+        }
+        change_amount = (utxo["amount"] * COIN - amount_to_burn - fee) / COIN
+        if change_amount > 0.0001:
+            tx_output_change = {
+                change_address: change_amount
+            }
+            return [tx_input], [tx_output_staking_burn_header, tx_output_change]
+        else:
+            return [tx_input], [tx_output_staking_burn_header]
+
+    def send_staking_burn_tx(self, change_address: str, amount_to_burn: decimal.Decimal, node_num: int):
+        unspent = self.nodes[node_num].listunspent()[0]
+        assert unspent[
+                   "amount"] * COIN > amount_to_burn, f"Amount too big ({amount_to_burn}) to be sent from this UTXO " \
+                                                      f"({unspent['amount'] * COIN})"
+        tx_inputs, tx_outputs = self.create_staking_burn_tx_input(change_address, amount_to_burn, unspent)
+        # create, sign and send staking burn transaction
+        raw_tx = self.nodes[node_num].createrawtransaction(tx_inputs, tx_outputs)
+        signed_raw_tx = self.nodes[node_num].signrawtransactionwithwallet(raw_tx)
+        self.nodes[0].sendrawtransaction(signed_raw_tx['hex'])
+
     def staking_burn_simple_test(self):
         starting_height = 200
         staking_reward = 50 * COIN
@@ -27,7 +61,8 @@ class StakingBurnTest(BitcoinTestFramework):
 
         node0_height = self.nodes[0].getblockcount()
         node1_height = self.nodes[1].getblockcount()
-        assert node0_height == node1_height == starting_height, f'Starting nodes height different than {starting_height, node0_height, node1_height}'
+        assert node0_height == node1_height == starting_height, f'Starting nodes height different than ' \
+                                                                f'{starting_height, node0_height, node1_height}'
 
         # generate 10 blocks on node 0, then sync nodes and check height and staking balance
         addr1 = self.nodes[0].getnewaddress()
@@ -43,35 +78,22 @@ class StakingBurnTest(BitcoinTestFramework):
         node1_height = self.nodes[1].getblockcount()
         assert node0_height == node1_height == (starting_height + 10), 'Difference in nodes height'
 
-        # gather data for staking burn transaction generation
-        # tx input (expected to be 450 ELC)
-        unspent = self.nodes[0].listunspent()[0]
-        tx_input = {
-            "txid": unspent["txid"],
-            "vout": unspent["vout"]
-        }
-
-        # tx outputs
-        amount_to_burn_bin = int(amount_to_burn).to_bytes(8, "little")
-        tx_output_staking_burn_header = {
-            "data": (bytes([STAKING_TX_HEADER, STAKING_TX_BURN_SUBHEADER]) + amount_to_burn_bin).hex()
-        }
-        tx_output_change = {
-            addr1: 49.99
-        }
-        # create, sign and send staking burn transaction
-        raw_tx = self.nodes[0].createrawtransaction([tx_input], [tx_output_staking_burn_header, tx_output_change])
-        signed_raw_tx = self.nodes[0].signrawtransactionwithwallet(raw_tx)
-        self.nodes[0].sendrawtransaction(signed_raw_tx['hex'])
+        # send staking burn transaction
+        self.send_staking_burn_tx(addr1, amount_to_burn, node_num=0)
 
         # check if staking pool balance wasn't increased yet
-        assert self.get_staking_pool_balance(node_num=0) == node0_staking_balance
+        assert self.get_staking_pool_balance(
+            node_num=0) == node0_staking_balance, f"Staking pool balance increased before a block has been mined"
 
         # mine a block and check if staking pool balance increased by a proper value
         self.nodes[0].generate(1)
         self.sync_all()
+        node0_expected_staking_pool_balance = node0_staking_balance + amount_to_burn + staking_reward
         assert self.get_staking_pool_balance(node_num=0) == self.get_staking_pool_balance(node_num=1) == \
-               node0_staking_balance + amount_to_burn + staking_reward
+               node0_expected_staking_pool_balance, f"Node 0 staking pool balance " \
+                                                    f"({self.get_staking_pool_balance(node_num=0)}) " \
+                                                    f"did not increase to expected value: " \
+                                                    f"{node0_expected_staking_pool_balance}"
 
     def staking_burn_reorg_test(self):
         starting_height = 200
@@ -81,12 +103,14 @@ class StakingBurnTest(BitcoinTestFramework):
 
         node0_height = self.nodes[0].getblockcount()
         node1_height = self.nodes[1].getblockcount()
-        assert node0_height == node1_height == starting_height, f'Starting nodes height different than {starting_height, node0_height, node1_height}'
+        assert node0_height == node1_height == starting_height, f'Starting nodes height different than ' \
+                                                                f'{starting_height, node0_height, node1_height}'
 
         # assure that staking balance is the same on both nodes
         node0_staking_balance = self.get_staking_pool_balance(node_num=0)
         node1_staking_balance = self.get_staking_pool_balance(node_num=1)
-        assert node0_staking_balance == node1_staking_balance == starting_staking_balance, f'Starting staking balance different than {starting_staking_balance}'
+        assert node0_staking_balance == node1_staking_balance == starting_staking_balance, \
+            f'Starting staking balance different than {starting_staking_balance}'
 
         # generate 10 blocks on node 0, then sync nodes and check height and staking balance
         addr1 = self.nodes[0].getnewaddress()
@@ -100,39 +124,32 @@ class StakingBurnTest(BitcoinTestFramework):
         disconnect_nodes(self.nodes[0], 1)
         disconnect_nodes(self.nodes[1], 2)
 
-        # gather data for staking burn transaction generation
-        # tx input (expected to be 450 ELC)
-        unspent = self.nodes[0].listunspent()[0]
-        tx_input = {
-            "txid": unspent["txid"],
-            "vout": unspent["vout"]
-        }
-
-        # tx outputs
-        amount_to_burn_bin = int(amount_to_burn).to_bytes(8, "little")
-        tx_output_staking_burn_header = {
-            "data": (bytes([STAKING_TX_HEADER, STAKING_TX_BURN_SUBHEADER]) + amount_to_burn_bin).hex()
-        }
-        tx_output_change = {
-            addr1: 149.99
-        }
-        # create, sign and send staking burn transaction
-        raw_tx = self.nodes[0].createrawtransaction([tx_input], [tx_output_staking_burn_header, tx_output_change])
-        signed_raw_tx = self.nodes[0].signrawtransactionwithwallet(raw_tx)
-        self.nodes[0].sendrawtransaction(signed_raw_tx['hex'])
+        # send staking burn transaction at node 0
+        self.send_staking_burn_tx(addr1, amount_to_burn, node_num=0)
 
         # check if staking pool balance wasn't increased yet
-        assert self.get_staking_pool_balance(node_num=0) == node0_staking_balance
+        assert self.get_staking_pool_balance(
+            node_num=0) == node0_staking_balance, f"Staking pool balance increased before a block has been mined"
 
         # mine some blocks, more on node 1 than 0
         self.nodes[0].generate(1)
         self.nodes[1].generate(2)
 
         # check if staking pool balance on node 0 has increased by a burned amount
-        assert self.get_staking_pool_balance(node_num=0) == node0_staking_balance + amount_to_burn + staking_reward
+        node0_expected_staking_pool_balance = node0_staking_balance + amount_to_burn + 1 * staking_reward
+        assert self.get_staking_pool_balance(
+            node_num=0) == node0_expected_staking_pool_balance, f"Node 0 staking pool balance " \
+                                                                f"({self.get_staking_pool_balance(node_num=0)}) " \
+                                                                f"did not increase to expected value: " \
+                                                                f"{node0_expected_staking_pool_balance}"
 
+        node1_expected_staking_pool_balance = node0_staking_balance + 2 * staking_reward
         # check if staking pool balance on node 1 hasn't increased by a burned amount
-        assert self.get_staking_pool_balance(node_num=1) == node0_staking_balance + 2 * staking_reward
+        assert self.get_staking_pool_balance(
+            node_num=1) == node1_expected_staking_pool_balance, f"Node 1 staking pool balance " \
+                                                                f"({self.get_staking_pool_balance(node_num=1)}) " \
+                                                                f"did not increase to expected value: " \
+                                                                f"{node1_expected_staking_pool_balance}"
 
         # connect nodes
         connect_nodes(self.nodes[0], 1)
@@ -140,7 +157,11 @@ class StakingBurnTest(BitcoinTestFramework):
         self.sync_all()
 
         # check if staking pool balance on node 1 hasn't increased by a burned amount
-        assert self.get_staking_pool_balance(node_num=0) == self.get_staking_pool_balance(node_num=1) == node0_staking_balance + 2 * staking_reward
+        assert self.get_staking_pool_balance(node_num=0) == self.get_staking_pool_balance(
+            node_num=1) == node1_expected_staking_pool_balance, f"Staking pool balances do not match: " \
+                                                                f"node 0: {self.get_staking_pool_balance(node_num=1)}, " \
+                                                                f"node 1: {self.get_staking_pool_balance(node_num=1)}, " \
+                                                                f"expected: {node1_expected_staking_pool_balance} "
 
 
 if __name__ == '__main__':
